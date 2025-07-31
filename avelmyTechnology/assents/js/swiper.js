aa595072dd88c507cd05f82c50126aa169e4629e
Edivaldo Junior <148983172+JuniorAvelar@users.noreply.github.com>
2025-07-30 21:16:14 -0300

    feat: implementa logica que ativa ou desativa botaõ ao fim do slider

--- avelmyTechnology/assents/js/swiper.js
+++ avelmyTechnology/assents/js/swiper.js
@@ -1,20 +1,73 @@
-   var swiper = new Swiper('.swiper', {
-      slidesPerView: 3,
-      direction: getDirection(),
-      navigation: {
-        nextEl: '.prev',
-        prevEl: '.next',
-      },
-      on: {
-        resize: function () {
-          swiper.changeDirection(getDirection());
-        },
-      },
-    });
+var swiper = new Swiper('.swiper', {
+slidesPerView: 3,
+direction: getDirection(),
+navigation: {
+  nextEl: '.next',
+  prevEl: '.prev',
+},
+on: {
+  resize: function () {
+    swiper.changeDirection(getDirection());
+  },
+  reachEnd(){
+    // document.querySelector('.next').classList.add("disable")
+    // document.querySelector('.prev').classList.remove("disable")
+    // console.log('fim')
+  },
+  reachBeginning(){
+    // document.querySelector('.prev').classList.add("disable")
+    // document.querySelector('.next').classList.remove("disable")
+  },
+    fromEdge: function () {
+    // Remove feedback quando não está mais no início ou fim
+    document.querySelector('.next').classList.remove('disabled');
+    document.querySelector('.prev').classList.remove('disabled');
+  }
+},  
+});
 
-    function getDirection() {
-      var windowWidth = window.innerWidth;
-      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+// logica que ativa e desativa botões do slider
+const cards = document.querySelectorAll(".swiper-slide")
+console.log(cards.length)
+let contador = (cards.length - 3)
 
-      return direction;
-    }
+document.addEventListener('DOMContentLoaded' , () =>{
+  if(contador == cards.length - 3) {
+    console.log('disable')
+    document.querySelector('.prev').classList.add("disable")
+  }
+})
+
+// button next
+const nextBtn = document.querySelector('.next').addEventListener("click" , () => {
+  contador -= 1
+  if(contador != cards.length - 3) {
+    document.querySelector('.prev').classList.remove("disable")
+  }
+
+  console.log(contador)
+  if(contador ==  0 ) {
+    document.querySelector('.next').classList.add("disable")
+  }
+})
+
+// button prev
+  const prevBtn = document.querySelector(".prev").addEventListener('click' , () => {
+  contador += 1
+  if(contador > 0) {
+    document.querySelector('.next').classList.remove("disable")
+  }
+
+  if(contador == cards.length - 3) {
+    console.log('disable')
+    document.querySelector('.prev').classList.add("disable")
+  }
+})
+
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
